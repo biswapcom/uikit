5b09054a67f976008beb22d12db1ad74802b2353
vadimmarkov <haha-mail@pm.me>
2021-04-07 16:33:31 +0300

    update component Banner

--- src/components/Banner/index.stories.tsx
+++ src/components/Banner/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 /* eslint-disable import/no-unresolved */
 import { Meta } from "@storybook/react/types-6-0";
-import styled from "styled-components";
 import { Button } from "../Button";
 
 import Banner from "./Banner";
@@ -23,16 +22,32 @@ export default {
   },
 } as Meta;
 
+const Slider: React.FC = () => {
+  return <div>Slider</div>;
+};
+
 export const Default: React.FC = ({ data, ...args }) => {
   return (
-    <div style={{ padding: "32px", width: "350px" }}>
-      <Banner>
-        <BannerTitle>{args.title}</BannerTitle>
-        <BannerBody>{args.body}</BannerBody>
-        <Button variant="danger" scale="sm">
-          Start farming
-        </Button>
-      </Banner>
-    </div>
+    <>
+      <div style={{ padding: "32px", width: "350px" }}>
+        <Banner>
+          <BannerTitle>{args.title}</BannerTitle>
+          <BannerBody>{args.body}</BannerBody>
+          <Button variant="danger" scale="sm">
+            Start farming
+          </Button>
+        </Banner>
+      </div>
+      <div style={{ padding: "32px", width: "900px" }}>
+        <Banner large>
+          <BannerTitle>{args.title}</BannerTitle>
+          <BannerBody>{args.body}</BannerBody>
+          <Button variant="danger" scale="sm">
+            Start farming
+          </Button>
+          <Slider />
+        </Banner>
+      </div>
+    </>
   );
 };
