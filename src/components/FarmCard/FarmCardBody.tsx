30e5a5133afbb3c06509f2ed4779168b3d9b2989
vadimmarkov <haha-mail@pm.me>
2021-04-08 17:11:46 +0300

    add component FarmCard

---
+++ src/components/FarmCard/FarmCardBody.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+interface FarmCardBodyProps {
+  isSoon?: boolean;
+  isNew?: boolean;
+}
+
+const StyledFarmCardBody = styled.div`
+  /* padding: 32px; */
+  /* border-radius: 24px; */
+  /* background-color: ${({ theme }) => theme.colors.contrast}; */
+  /* background-color: #fff; */
+  /* display: flex; */
+
+  display: flex;
+  align-items: center;
+
+  & > p {
+    flex: 1 1;
+    color: ${({ theme }) => theme.colors.primary};
+    font-size: 24px;
+    font-weight: 600;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+`;
+
+const FarmCardBody: React.FC<FarmCardBodyProps> = ({ children }) => {
+  return <StyledFarmCardBody isDark={true}>{children}</StyledFarmCardBody>;
+};
+
+export default FarmCardBody;
