bd29a02609ff698319f8c8cd8092fcfb4903cd77
yaroslav <yaroslav.vinnik27@gmail.com>
2021-05-16 16:52:20 +0300

    fix menu

--- dist/widgets/Menu/components/Footer.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { BSWPriceProps, FooterSupplyProps, FooterTotalProps } from "../types";
-interface Props extends BSWPriceProps, FooterSupplyProps, FooterTotalProps {
-    isPushed: boolean;
-}
-declare const Footer: React.FC<Props>;
-export default Footer;
