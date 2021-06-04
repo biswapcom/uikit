0d2dd68fd2a90d7e7f2718ae8f86e1788d9bb223
Malyuk05 <sashamalyuk8@gmail.com>
2021-06-04 12:54:03 +0300

    test 2

---
+++ dist/widgets/WalletModal/AccountModal.d.ts
@@ -0,0 +1,13 @@
+import React from "react";
+interface Props {
+    account: string;
+    logout: () => void;
+    onDismiss?: () => void;
+    login?: any;
+    recentTransaction?: any;
+    rowStatus?: any;
+    chainId?: any;
+    getRowStatus?: any;
+}
+declare const AccountModal: React.FC<Props>;
+export default AccountModal;
