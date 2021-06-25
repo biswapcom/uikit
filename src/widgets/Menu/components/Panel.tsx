887a9550a9d79796b252c3d64b1e6aeda3a33007
front_y <yaroslav.v@lineir.me>
2021-06-25 11:25:38 +0300

    test fix panel

--- src/widgets/Menu/components/Panel.tsx
+++ src/widgets/Menu/components/Panel.tsx
@@ -26,8 +26,8 @@ const StyledPanel = styled.div<{ isPushed: boolean; showMenu: boolean }>`
   flex-shrink: 0;
   background-color: ${({ theme }) => theme.nav.background};
   width: ${({ isPushed }) => (isPushed ? `${SIDEBAR_WIDTH_FULL}px` : 0)};
-  height: calc(100vh - 64px);
-  max-height: -webkit-fill-available;
+  //height: calc(100vh - 64px);
+  height: -webkit-fill-available;
   transition: padding-top 0.2s, width 0.2s;
   border-right: ${({ isPushed }) => (isPushed ? "2px solid rgba(133, 133, 133, 0.1)" : 0)};
   overflow: ${({ isPushed }) => (isPushed ? "initial" : "hidden")};
