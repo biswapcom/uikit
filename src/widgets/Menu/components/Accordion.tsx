8ff08b1665d8db2e23b0a31e6cc4bf9a42fd1563
Malyuk05 <sashamalyuk8@gmail.com>
2021-07-15 12:25:50 +0300

    try 1

--- src/widgets/Menu/components/Accordion.tsx
+++ src/widgets/Menu/components/Accordion.tsx
@@ -1,18 +1,20 @@
 import React, { ReactNode, useState } from "react";
 import styled from "styled-components";
 import { MENU_ENTRY_HEIGHT } from "../config";
-import { LinkLabel, MenuEntry } from "./MenuEntry";
-import { PushedProps } from "../types";
+import { LinkLabel, LinkStatus as LinkStatusComponent, MenuEntry } from "./MenuEntry";
+import { LinkStatus, PushedProps } from "../types";
 import { ArrowDropDownIcon, ArrowDropUpIcon } from "../../../components/Svg";
 
 interface Props extends PushedProps {
   label: string;
+  status?: LinkStatus;
   icon: React.ReactElement;
   initialOpenState?: boolean;
   className?: string;
   children: ReactNode;
   isActive?: boolean;
 }
+
 
 const Container = styled.div`
   display: flex;
@@ -25,23 +27,23 @@ const AccordionContent = styled.div<{ isOpen: boolean; isPushed: boolean; maxHei
   max-height: ${({ isOpen, maxHeight }) => (isOpen ? `${maxHeight}px` : 0)};
   transition: max-height 0.3s ease-out;
   overflow: hidden;
-  // border-color: ${({ isOpen, isPushed }) => (isOpen && isPushed ? "rgba(133, 133, 133, 0.1)" : "transparent")};
-  //border-style: solid;
-  //border-width: 1px 0;
+  border-color: ${({ isOpen, isPushed }) => (isOpen && isPushed ? "rgba(133, 133, 133, 0.1)" : "transparent")};
+  border-style: solid;
+  border-width: 1px 0;
 `;
 
 const Accordion: React.FC<Props> = ({
-  label,
-  icon,
-  isPushed,
-  pushNav,
-  initialOpenState = false,
-  children,
-  className,
-  isActive,
-}) => {
+                                      label,
+                                      status,
+                                      icon,
+                                      isPushed,
+                                      pushNav,
+                                      initialOpenState = false,
+                                      children,
+                                      className,
+                                      isActive,
+                                    }) => {
   const [isOpen, setIsOpen] = useState(initialOpenState);
-
   const handleClick = () => {
     if (isPushed) {
       setIsOpen((prevState) => !prevState);
@@ -53,9 +55,14 @@ const Accordion: React.FC<Props> = ({
 
   return (
     <Container>
-      <MenuEntry onClick={handleClick} className={className} isActive={isActive}>
+      <MenuEntry onClick={handleClick} className={className} isActive={isActive} role="button">
         {icon}
         <LinkLabel isPushed={isPushed}>{label}</LinkLabel>
+        {status && (
+          <LinkStatusComponent color={status.color} fontSize="14px">
+            {status.text}
+          </LinkStatusComponent>
+        )}
         {isOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
       </MenuEntry>
       <AccordionContent
@@ -69,4 +76,4 @@ const Accordion: React.FC<Props> = ({
   );
 };
 
-export default React.memo(Accordion, (prev, next) => prev.isPushed === next.isPushed);
+export default Accordion;
