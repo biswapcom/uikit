8de76549eb6c9f5b93e59a68850933dce97706d8
front O <oleg.k@lineir.me>
2021-08-05 12:45:51 +0300

    math wallet log

--- src/widgets/WalletModal/WalletCard.tsx
+++ src/widgets/WalletModal/WalletCard.tsx
@@ -18,6 +18,7 @@ const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss, mb }) => 
       width="100%"
       variant="tertiary"
       onClick={() => {
+        console.log(walletConfig);
         login(walletConfig.connectorId);
         window.localStorage.setItem(connectorLocalStorageKey, walletConfig.connectorId);
         onDismiss();
--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,5 +1,5 @@
 import Metamask from "./icons/Metamask";
-// import MathWallet from "./icons/MathWallet";
+import MathWallet from "./icons/MathWallet";
 // import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
@@ -18,11 +18,11 @@ const connectors: Config[] = [
    icon: TrustWallet,
    connectorId: ConnectorNames.Injected,
   },
-  // {
-  //   title: "MathWallet",
-  //   icon: MathWallet,
-  //   connectorId: ConnectorNames.Injected,
-  // },
+  {
+    title: "MathWallet",
+    icon: MathWallet,
+    connectorId: ConnectorNames.Injected,
+  },
   // {
   //   title: "TokenPocket",
   //   icon: TokenPocket,
