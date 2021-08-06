e02b55d44a368875548fcf61633181f7bdef595b
front O <oleg.k@lineir.me>
2021-08-06 15:02:31 +0300

    add coin98

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,10 +1,11 @@
 import Metamask from "./icons/Metamask";
 import MathWallet from "./icons/MathWallet";
-import TokenPocket from "./icons/TokenPocket";
+// import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
 import BinanceChain from "./icons/BinanceChain";
 import SafePalWallet from "./icons/SafePalWallet";
+import Coin98 from "./icons/Coin98";
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
@@ -23,9 +24,14 @@ const connectors: Config[] = [
     icon: MathWallet,
     connectorId: ConnectorNames.Injected,
   },
+  // {
+  //   title: "TokenPocket",
+  //   icon: TokenPocket,
+  //   connectorId: ConnectorNames.Injected,
+  // },
   {
-    title: "TokenPocket",
-    icon: TokenPocket,
+    title: "Coin98",
+    icon: Coin98,
     connectorId: ConnectorNames.Injected,
   },
   {
