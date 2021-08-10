282adf4aaa86f508bd69b356b7300381ecbd50e7
front O <oleg.k@lineir.me>
2021-08-10 14:37:14 +0300

    only token pocket

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,11 +1,11 @@
 import Metamask from "./icons/Metamask";
-import MathWallet from "./icons/MathWallet";
+// import MathWallet from "./icons/MathWallet";
 import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
 import BinanceChain from "./icons/BinanceChain";
 import SafePalWallet from "./icons/SafePalWallet";
-import Coin98 from "./icons/Coin98";
+// import Coin98 from "./icons/Coin98";
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
@@ -19,21 +19,21 @@ const connectors: Config[] = [
     icon: TrustWallet,
     connectorId: ConnectorNames.Injected,
   },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: ConnectorNames.Injected,
-  },
+  // {
+  //   title: "MathWallet",
+  //   icon: MathWallet,
+  //   connectorId: ConnectorNames.Injected,
+  // },
   {
     title: "TokenPocket",
     icon: TokenPocket,
     connectorId: ConnectorNames.Injected,
   },
-  {
-    title: "Coin98",
-    icon: Coin98,
-    connectorId: ConnectorNames.Injected,
-  },
+  // {
+  //   title: "Coin98",
+  //   icon: Coin98,
+  //   connectorId: ConnectorNames.Injected,
+  // },
   {
     title: "WalletConnect",
     icon: WalletConnect,
