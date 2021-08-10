f441b5f52b6a4696c48627b0a9249eae1caf21b2
front O <oleg.k@lineir.me>
2021-08-10 14:29:48 +0300

    only MathWallet

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,5 +1,5 @@
 import Metamask from "./icons/Metamask";
-// import MathWallet from "./icons/MathWallet";
+import MathWallet from "./icons/MathWallet";
 // import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
@@ -14,24 +14,24 @@ const connectors: Config[] = [
     connectorId: ConnectorNames.Injected,
   },
   {
-  title: "TrustWallet",
-   icon: TrustWallet,
-   connectorId: ConnectorNames.Injected,
+    title: "TrustWallet",
+    icon: TrustWallet,
+    connectorId: ConnectorNames.Injected,
   },
-  // {
-  //   title: "MathWallet",
-  //   icon: MathWallet,
-  //   connectorId: ConnectorNames.Injected,
-  // },
+  {
+    title: "MathWallet",
+    icon: MathWallet,
+    connectorId: ConnectorNames.Injected,
+  },
   // {
   //   title: "TokenPocket",
   //   icon: TokenPocket,
   //   connectorId: ConnectorNames.Injected,
   // },
   {
-  title: "WalletConnect",
-  icon: WalletConnect,
-  connectorId: ConnectorNames.WalletConnect,
+    title: "WalletConnect",
+    icon: WalletConnect,
+    connectorId: ConnectorNames.WalletConnect,
   },
   {
     title: "Binance Chain Wallet",
