a536b1d7e1aab52ec6de4e37b17521fa2ea133d1
front_O <oleg.k@lineir.me>
2021-08-25 11:59:01 +0300

    add math wallet

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,11 +1,10 @@
 import Metamask from "./icons/Metamask";
-// import MathWallet from "./icons/MathWallet";
+import MathWallet from "./icons/MathWallet";
 // import TokenPocket from "./icons/TokenPocket";
 import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
 import BinanceChain from "./icons/BinanceChain";
 import SafePalWallet from "./icons/SafePalWallet";
-// import MathWallet from "./icons/MathWallet";
 import Coin98 from "./icons/Coin98";
 
 import { Config, ConnectorNames } from "./types";
@@ -23,12 +22,12 @@ const connectors: Config[] = [
     connectorId: ConnectorNames.Injected,
     priority: 2,
   },
-  // {
-  //   title: "MathWallet",
-  //   icon: MathWallet,
-  //   connectorId: ConnectorNames.Injected,
-  //   priority: 3,
-  // },
+  {
+    title: "MathWallet",
+    icon: MathWallet,
+    connectorId: ConnectorNames.Injected,
+    priority: 999,
+  },
   // {
   //   title: "TokenPocket",
   //   icon: TokenPocket,
